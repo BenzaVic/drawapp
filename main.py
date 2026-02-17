bc1879df01b607d9763e6b2c822c90ae87393a0c
Benjamin Vicente <bj016222@student.reading.ac.uk>
2026-02-17 00:49:25 +0000

    Add canvas ui

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 
+root = tk.Tk()
+root.title("Drawing App")
+
+canvas = tk.Canvas(root, width=400, height=400, bg="white")
+canvas.pack(fill=tk.BOTH, expand=True)   
+
+root.mainloop()
